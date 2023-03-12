d4bc7fa6f0b7191701b6e389c6d2f48f126eee12
vinniiee <vs28245@gmail.com>
2023-03-12 09:29:08 +0530

    configure env

--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -10,40 +10,35 @@ import { removeBookmark } from "../utils/requests/bookmarks/removeBookmark";
 
 export default function WeatherCard(props) {
   const [weather, setWeather] = useState();
-  const [saved,setSaved] = useState("");
+  const [saved, setSaved] = useState("");
 
   useEffect(() => {
     const getWeather = async () => {
       const info = await getWeatherInfo(props.city);
       const isBookmarked = await checkBookmark(props.city);
-      setSaved(isBookmarked?"active":"");
+
+      setSaved(isBookmarked ? "active" : "");
       if (!info) {
         props.setSearchItem([]);
       } else {
-        // console.log(info);
         setWeather(info.weather);
       }
     };
     getWeather();
   }, [props, setWeather]);
 
-  const bookmarkHandler = async ()=>{
-      // const newCity = new City({...props.city})
-      console.log({...props.city});
-      if(saved.length===0){
-        const savedBookmark = await saveBookmark(props.city,props.setBookmarks);
-        if(savedBookmark){
-          console.log("Saved Bookmark: ",savedBookmark);
-          setSaved("active");
-        }
-      }else{
-        const deleted = await removeBookmark(props.city,props.setBookmarks);
-        setSaved(deleted?"":"active");
+  const bookmarkHandler = async () => {
+    if (saved.length === 0) {
+      const savedBookmark = await saveBookmark(props.city, props.setBookmarks);
+      if (savedBookmark) {
+        console.log("Saved Bookmark: ", savedBookmark);
+        setSaved("active");
       }
-
-  }
-
-
+    } else {
+      const deleted = await removeBookmark(props.city, props.setBookmarks);
+      setSaved(deleted ? "" : "active");
+    }
+  };
 
   const [temperatureUnit, setTemperatureUnit] = useState(false);
   const [windUnit, setWindUnit] = useState(true);
@@ -71,64 +66,65 @@ export default function WeatherCard(props) {
       Mi/Hr
     </span>
   );
-  // console.log("@@@@@@@@@@",weather);
   return (
     <React.Fragment>
       <div className="weather-card-wrap">
-      <div className="weather-card">
-        <div className="heading-wrap">
-          <div className="weather-card-primary-heading">
-            <ImLocation2 className="location-icon" />
-            <p className="heading-text margin-left-1">
-              {props.city.name}
-              {props.city.state ? ", " + props.city.state : ""},{" "}
-              {props.city.country}
-            </p>
-          </div>
-          <BsFillBookmarkFill className={`bookmark-icon-card ${saved}`} onClick={bookmarkHandler}/>
-        </div>
-        <div className="partition"></div>
-        <div className="weather-info">
-          <div className="weather-info-item temperature">
-            <div className="info-item-title">
-              <BsThermometerSun className="info-item-icon" />
-              <span className="info-item-title-text text">Temperature</span>
+        <div className="weather-card">
+          <div className="heading-wrap">
+            <div className="weather-card-primary-heading">
+              <ImLocation2 className="location-icon" />
+              <p className="heading-text margin-left-1">
+                {props.city.name}
+                {props.city.state ? ", " + props.city.state : ""},{" "}
+                {props.city.country}
+              </p>
             </div>
-            <span className="info-item-value temperature-value">
-              {weather !== undefined &&
-                (temperatureUnit
-                  ? weather.temp.farenheit
-                  : weather.temp.celcius)}{" "}
-              {tempUnitDisplay}
-            </span>
+            <BsFillBookmarkFill
+              className={`bookmark-icon-card ${saved}`}
+              onClick={bookmarkHandler}
+            />
           </div>
           <div className="partition"></div>
-          <div className="weather-info-item humidity">
-            <div className="info-item-title">
-              <GiDroplets className="info-item-icon humidity-icon" />
-              <span className="info-item-title-text text">Humidity</span>
+          <div className="weather-info">
+            <div className="weather-info-item temperature">
+              <div className="info-item-title">
+                <BsThermometerSun className="info-item-icon" />
+                <span className="info-item-title-text text">Temperature</span>
+              </div>
+              <span className="info-item-value temperature-value">
+                {weather !== undefined &&
+                  (temperatureUnit
+                    ? weather.temp.farenheit
+                    : weather.temp.celcius)}{" "}
+                {tempUnitDisplay}
+              </span>
             </div>
-            <span className="info-item-value humidity-value">
-              {weather !== undefined && weather.humidity} %
-            </span>
-          </div>
-          <div className="partition"></div>
-          <div className="weather-info-item wind">
-            <div className="info-item-title">
-              <BsWind className="info-item-icon wind-icon" />
-              <span className="info-item-title-text text">Wind Speed</span>
+            <div className="partition"></div>
+            <div className="weather-info-item humidity">
+              <div className="info-item-title">
+                <GiDroplets className="info-item-icon humidity-icon" />
+                <span className="info-item-title-text text">Humidity</span>
+              </div>
+              <span className="info-item-value humidity-value">
+                {weather !== undefined && weather.humidity} %
+              </span>
             </div>
-            <span className="info-item-value wind-value">
-              {weather !== undefined &&
-                (windUnit ? weather.wind.kmph : weather.wind.mph)}{" "}
-              {windUnitDisplay}
-            </span>
+            <div className="partition"></div>
+            <div className="weather-info-item wind">
+              <div className="info-item-title">
+                <BsWind className="info-item-icon wind-icon" />
+                <span className="info-item-title-text text">Wind Speed</span>
+              </div>
+              <span className="info-item-value wind-value">
+                {weather !== undefined &&
+                  (windUnit ? weather.wind.kmph : weather.wind.mph)}{" "}
+                {windUnitDisplay}
+              </span>
+            </div>
+            <div className="partition"></div>
           </div>
-          <div className="partition"></div>
         </div>
       </div>
-    </div>
-    
     </React.Fragment>
   );
 }
--- src/utils/requests/bookmarks/server.js
+++ src/utils/requests/bookmarks/server.js
@@ -1,1 +1,3 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
 export const server = process.env.API_URI||"http://localhost:3001";
